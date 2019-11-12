02860afaa8692a77567c204172f8368de20123f6
14rau <patrickrau@kabelmail.de>
2019-11-12 11:07:51 +0100

    merge

--- src/Components/Game/Game.tsx
+++ src/Components/Game/Game.tsx
@@ -1,7 +1,32 @@
 import * as React from "react";
+import { inject, observer } from "mobx-react"
+import Unity, { UnityContent } from "react-unity-webgl";
 
+
+@inject("api")
+@observer
 export class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // include this later
+    /*
+    this.unityContent = new UnityContent(
+      "MyGame/Build.json",
+      "MyGame/UnityLoader.js"
+    );
+    */
+     
+  }
+
+  // replace div with id unity with:
+  /*
+    <Unity unityContent={unityContent} />;
+  */
   public render() {
-    return <div>Hello Game</div>;
+    return <div>
+
+    </div>;
   }
 }

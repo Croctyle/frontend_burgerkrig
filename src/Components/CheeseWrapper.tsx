11071e6c9b330212128b2d8ea62effb0fb6111bb
Simon Somerville <simon.somerville@tuleva.de>
2019-11-13 14:22:30 +0100

    Profil

---
+++ src/Components/CheeseWrapper.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+interface ICheeseWrapperProps {
+  style?: React.CSSProperties;
+}
+
+export class CheeseWrapper extends React.Component<ICheeseWrapperProps> {
+  public render() {
+    let style: React.CSSProperties = {
+      background: "#ffc71e",
+      borderRadius: "30%",
+      border: "11px solid rgb(255,171,19)",
+      color: "lightyellow"
+    };
+
+    style = { ...style, ...(this.props.style || {}) };
+
+    return <div style={style}>{this.props.children}</div>;
+  }
+}
+
+/*
+<CheeseWrapper>
+
+</CheeseWeapper>
+*/

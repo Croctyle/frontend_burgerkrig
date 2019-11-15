cda1361a51533cce74a8770c5b7965a66521fefa
Simon Somerville <simon.somerville@tuleva.de>
2019-11-15 12:45:10 +0100

    rankinfo

--- src/Components/Profil.tsx
+++ src/Components/Profil.tsx
@@ -3,7 +3,9 @@ import { inject } from "mobx-react";
 import { Api } from "../Api";
 import { CheeseWrapper } from "./CheeseWrapper";
 
-export interface IProfilState {}
+export interface IProfilState {
+  user: any;
+}
 
 export interface IProfilProps {
   id: number;
@@ -16,10 +18,19 @@ export interface IProfilProps {
 export class Profil extends React.Component<IProfilProps, IProfilState> {
   constructor(props: IProfilProps) {
     super(props);
-    this.state = {};
+    this.state = { user: null };
+  }
+
+  public async componentDidMount() {
+    let user = await this.props.api.request("user.getById", {
+      userId: this.props.id || this.props.api.userId
+    });
+    console.log(user);
+    this.setState({ user });
   }
 
   public render(): JSX.Element {
+    if (!this.state.user) return <div />;
     return (
       <div style={{ display: "flex" }}>
         <CheeseWrapper
@@ -49,13 +60,50 @@ export class Profil extends React.Component<IProfilProps, IProfilState> {
             id="stats"
             style={{ fontFamily: "showcard gothic", marginTop: "50px" }}
           >
-            <h3>Burger King</h3>
-            <h3>Abgefeuerte Gurken</h3>
-            <p>4398520352380</p>
-            <h3>Gesamt Spielzeit</h3>
-            <p>100min</p>
+            <h2>Dein Highscore</h2>
+            <h4>{this.state.user.gameinfo.highscore}</h4>
+            <h2>Abgefeuerte Gurken</h2>
+            <h4></h4>
+            <h2>Gesamt Spielzeit</h2>
+            <h4>{this.state.user.gameinfo.timespend}</h4>
+            <h2>Gesammelte Punkte Gesamt</h2>
+            <h4>{this.state.user.gameinfo.points}</h4>
           </div>
         </CheeseWrapper>
+        <div>
+          <CheeseWrapper
+            style={{
+              width: "20%",
+              height: "20em",
+              margin: "auto",
+              marginTop: "8em",
+              textAlign: "center",
+              position: "absolute",
+              display: "inlineblock",
+              marginLeft: "-27em"
+            }}
+          >
+            <div
+              id="rankinfo"
+              style={{ fontFamily: "showcard gothic", marginTop: "30px" }}
+            >
+              <h2>Aktueller Rank</h2>
+              <h3 style={{ color: this.state.user.currentRank.rank_color }}>
+                {this.state.user.currentRank.rank_name}
+              </h3>
+              <h2>Nächster Rank</h2>
+              <h4 style={{ color: this.state.user.nextRank.color }}>
+                {this.state.user.nextRank.name}
+              </h4>
+              <h4>In</h4>
+              <h4>
+                {this.state.user.nextRank.points -
+                  this.state.user.gameinfo.points}{" "}
+                Points
+              </h4>
+            </div>
+          </CheeseWrapper>
+        </div>
       </div>
     );
   }
